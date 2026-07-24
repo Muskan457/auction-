edf3795ba1c809af33951977f2456b4a08dd176b
Navdeep Kaur <navdeep0470.becse24@chitkara.edu.in>
2026-07-24 11:54:14 +0530

    Add project changes

---
+++ java.js
@@ -0,0 +1,32 @@
+let search=document.querySelector('.searchbar');
+
+document.querySelector('#search').onclick = () => {
+    search.classList.toggle('active');
+    favorite.classList.remove('active');
+    login.classList.remove('active');
+}
+
+
+let favorite=document.querySelector('.favorite');
+
+document.querySelector('#favorite').onclick = () => {
+    favorite.classList.toggle('active');
+    search.classList.remove('active');
+    login.classList.remove('active');
+}
+
+
+let login=document.querySelector('.login');
+
+document.querySelector('#login').onclick = () => {
+    login.classList.toggle('active');
+    search.classList.remove('active');
+    favorite.classList.remove('active');
+}
+
+
+window.onscroll = () => {
+    search.classList.remove('active');
+    favorite.classList.remove('active');
+    login.classList.remove('active');
+};
